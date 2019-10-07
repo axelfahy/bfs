318d2b71370fb91ca5d52f0d6ae84fcd866b0169
Axel Fahy <axel.fahy@heig-vd.ch>
2019-10-07 10:33:14 +0200

    feat(jupyterlab): add script to run jupyterlab on server

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # bfs
-Best Fancy Scrips, your Best Friend Forever! 
+> Best Fancy Scripts, your Best Free Slave!
+
+This repository contains some useful scripts:
+
+* `run_jupyterlab.sh`: script to start [JupyterLab](https://jupyterlab.readthedocs.io/en/stable/) on a server. The script will automatically find an available port and display the correct url to access the notebook.
+
+## Meta
+
+Axel Fahy – axel@fahy.net
+
+Distributed under the MIT license. See ``LICENSE`` for more information.
+
+[https://github.com/axelfahy](https://github.com/axelfahy)
+
+## Contributing
+
+1. Fork it (<https://github.com/yourname/yourproject/fork>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
+
---
+++ scripts/run_jupyterlab.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: run_jupyterlab.sh
+#
+#         USAGE: ./run_jupyterlab.sh
+#
+#   DESCRIPTION: Script to start JupyterLab  on a server
+#                and display the right url, that is the one with the hostname
+#                of the server and the correct port used.
+#
+#                The script tries to find an available port starting from 8888.
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Axel Fahy (axel@fahy.net),
+#  ORGANIZATION:
+#       CREATED: 01.05.19 15:07
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error.
+
+declare -r HOST=`hostname`
+
+# Check if jupyterlab is installed.
+if ! python -c 'import pkgutil; exit(not pkgutil.find_loader("jupyterlab"))'; then
+    echo -e "Package jupyterlab is not installed or virtual environment is not activated. Aborting." 1>&2
+    exit 1
+fi
+
+echo -e "Possible Jupyter binaries: $(whereis jupyter)"
+echo -e "Using Jupyter from $(which jupyter)"
+
+echo -e "Looking for an available port..."
+
+declare port=8888
+
+for i in $(seq $port 8950); do
+    # Check if the port is already used.
+    echo -e "Checking port: $i"
+    ss -tlnp | grep -q $i &> /dev/null
+    if [ $? -ne 0 ]; then
+        port=$i
+        echo -e "Using port: $port"
+        break
+    fi
+done
+
+jupyter lab --ip=0.0.0.0 --NotebookApp.port=$port --NotebookApp.custom_display_url=http://$HOST:$port
+
